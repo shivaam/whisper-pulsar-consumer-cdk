7f9c0bd0cc3bb94a233623c5cc4ae8389718a80f
Shivam Rastogi <shivam.riky@gmail.com>
2024-02-21 21:04:08 -0800

    Updated code for github-token retrieval

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -48,9 +48,11 @@ export class PipelineStack extends Stack {
 
     const githubRepo = 'shivaam/whisper-pulsar-consumer-cdk';
 
-    const gitHubSource = CodePipelineSource.gitHub(githubRepo, "master", {
-      authentication: SecretValue.secretsManager("lambda_container_cdk_pipeline_github", { jsonField: 'github' }),
-    });
+    const gitHubSource = CodePipelineSource.gitHub(githubRepo, "master",
+      {
+        authentication: cdk.SecretValue.secretsManager('github-token')
+      }
+    );
 
     // A shell script step that describes a github source code and runs the commands on the source code to build the pipeline.
     // In the shell step we are also building the application code along with the pipeline code.
@@ -68,7 +70,7 @@ export class PipelineStack extends Stack {
     const githubAppRepo = 'shivaam/whisper-pulsar-consumer';
 
     const gitHubAppSource = CodePipelineSource.gitHub(githubAppRepo, "master", {
-      authentication: SecretValue.secretsManager("lambda_container_cdk_pipeline_github", { jsonField: 'github' }),
+      authentication: SecretValue.secretsManager("github-token"),
     });
 
     const buildContainerProject = new CodeBuildStep("ContainerBuild", {
